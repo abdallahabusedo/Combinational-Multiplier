80c30ec62e866ed078c11b18a03a333e9495fdc4
IslamAhmed1092 <eslam1092@hotmail.com>
2021-04-09 18:45:44 +0200

    the end

--- mult_integrated.v
+++ mult_integrated.v
@@ -1,11 +1,11 @@
 module mult_integrated (
-    	input [3:0] inA, inB,
-	output reg[6:0] HandredSeg,TenSeg,OnesSeg
+    	input [3:0] A, B,
+	output [6:0] HandredSeg,TenSeg,OnesSeg
 );
 	wire [3:0] TempH,TempO,TempT; 
     	
    	wire [7:0] Multiout;
-	mult4x4 F(inA,inB,Multiout);
+	mult4x4 F(A,B,Multiout);
 	bcd G (Multiout,TempH,TempT,TempO);
 	ssd Ha(TempH,HandredSeg);
 	ssd Te(TempT,TenSeg);
