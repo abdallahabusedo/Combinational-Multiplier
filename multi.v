9cc2474847f90d7552e6f523eaae3355293ccfdf
abdallahabusedo <abozoooz2014.aa@gmail.com>
2021-04-09 16:11:51 +0200

    fixing Multiplication

--- multi.v
+++ multi.v
@@ -1,8 +1,22 @@
-module multi ( 
-    input [3:0] ina,
-    input [3:0] inb,
-    output wire [7:0] outd 
-);
-   
-    assign outd = ina * inb;
+module multi (ina,inb,outd);
+    input [3:0] ina;
+    input [3:0] inb;
+
+    wire [3:0] L0;
+    wire [4:0] L1;
+    wire [5:0] L2;
+    wire [6:0] L3;
+    wire [7:0] Re1,Re2,Re3;
+    inout reg [7:0] outd;
+
+    assign L0 = {4{ina[0]}} &inb[3:0];
+    assign L1 = {4{ina[1]}} &inb[3:0];
+    assign L2 = {4{ina[2]}} &inb[3:0];
+    assign L3 = {4{ina[3]}} &inb[3:0];
+
+    assign Re1 = L0 + (L1<<1);
+    assign Re2 = Re1 + (L2<<2);
+    assign Re3 = Re2 + (L3<<3);
+    assign outd =Re3;
+
 endmodule //multi
